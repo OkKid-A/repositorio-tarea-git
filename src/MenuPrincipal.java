c57a17aeee6174b91d89354bc2d1d4e68a2a1456
Zafkiel <davidbarreno13@gmail.com>
2023-07-30 17:36:28 -0600

    Menu listo

---
+++ src/MenuPrincipal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public static void menuJuegos(){
+        Scanner sn = new Scanner(System.in);
+
+        boolean salir = false;
+        int opcion;
+        while (!salir){
+            System.out.println("|****************************************************|");
+            System.out.println("|                                                    |");
+            System.out.println("|                B I E N V E N I D O                 |");
+            System.out.println("|                                                    |");
+            System.out.println("|   Por favor seleccione un opcion a continuación,   |");
+            System.out.println("|                                                    |");
+            System.out.println("|   1 - Juego de Ahorcado.                           |");
+            System.out.println("|   2 - Juego de Basketball.                         |");
+            System.out.println("|   3 - Juego de Cartas.                             |");
+            System.out.println("|                                                    |");
+            System.out.println("|   4 - Salir.                                       |");
+            System.out.println("|                                                    |");
+            System.out.println("|   Hecho por 201830233.                             |");
+            System.out.println("|                                                    |");
+            System.out.println("|****************************************************|");
+
+            System.out.println("|   Su selección es:                                 |");
+            opcion = sn.nextInt();
+
+        }
+
+    }
+}
